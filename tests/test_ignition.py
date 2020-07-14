5cd659404115ac48305e45e45e04990b6333df6d
iKrishneel <krishneel.chand.chaudhary@gmail.com>
2020-07-14 12:27:28 +0900

    added test

---
+++ tests/test_ignition.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+import torch
+from torch.utils.data import DataLoader
+from torch import nn
+import torch.nn.functional as F
+from torch.optim import SGD
+from torchvision.transforms import Compose, ToTensor, Normalize
+
+from torch_ignition import TorchIgnite
+from .network import Net
+
+
+def test_ignite():
+    model = Net()
+    optim = SGD(model.parameters(), lr=0.01, momentum=0.5)
+    criterion = nn.NLLLoss()
+    t = TorchIgnite(model=model,
+                    optimizer=optim,
+                    criterion=criterion,
+                    dataloader=None)
+    
+    assert t.evaluator
+    assert t.trainer
+    assert t.writer
+    assert t.model == model
+    assert t.criterion == criterion
+    assert t.optimizer == optim
+
